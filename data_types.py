cf5c1618a680161d25ea6cacfe7853a1179058d2
Senpai <rostyslav.mogilnyi@gmail.com>
2022-10-23 13:02:36 +0300

    second homework

---
+++ data_types.py
@@ -0,0 +1,19 @@
+first_name = 'Ростисав'
+last_name = 'Могильний'
+age = 26
+home_addres = 'м.Бориспiль, вул.Пiтонова 42'
+personal_email = 'rostyslav.senpai@meta.ua'
+phone_number = '+380997961125'
+teeth_amount = 29
+visit_countdown = 6  # days until next visit
+doctor = 'Авраменко А.В.'
+patient_debt = 420
+
+print('Пацієнт:', first_name, last_name)
+print('Вiк:', age, 'повних рокiв')
+print('Контактна iнформацiя:', personal_email, ';', phone_number, ';')
+print('Адреса проживання:', home_addres)
+print('Закрiплений лiкар:', doctor)
+print('Зубiв залишилось:', teeth_amount)
+print('Днiв до наступного вiзиту:', visit_countdown)
+print('Борг перед клiнiкою:', patient_debt, 'грн')
