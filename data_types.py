6ff3cea18767b50b86c8c46ba9b6469138bf14bc
Senpai <rostyslav.mogilnyi@gmail.com>
2022-10-24 13:17:30 +0300

    second homework (retransmission)

--- data_types.py
+++ data_types.py
@@ -9,11 +9,11 @@ visit_countdown = 6  # days until next visit
 doctor = 'Авраменко А.В.'
 patient_debt = 420
 
-print('Пацієнт:', first_name, last_name)
-print('Вiк:', age, 'повних рокiв')
-print('Контактна iнформацiя:', personal_email, ';', phone_number, ';')
-print('Адреса проживання:', home_addres)
-print('Закрiплений лiкар:', doctor)
-print('Зубiв залишилось:', teeth_amount)
-print('Днiв до наступного вiзиту:', visit_countdown)
-print('Борг перед клiнiкою:', patient_debt, 'грн')
+print('Пацієнт:', first_name, type(first_name), last_name, type(last_name))
+print('Вiк:', age, 'повних рокiв', type(age))
+print('Контактна iнформацiя:', personal_email, type(personal_email), ';', phone_number, type(phone_number), ';')
+print('Адреса проживання:', home_addres, type(home_addres))
+print('Закрiплений лiкар:', doctor, type(doctor))
+print('Зубiв залишилось:', teeth_amount, type(teeth_amount))
+print('Днiв до наступного вiзиту:', visit_countdown, type(visit_countdown))
+print('Борг перед клiнiкою:', patient_debt, 'грн', type(patient_debt))
